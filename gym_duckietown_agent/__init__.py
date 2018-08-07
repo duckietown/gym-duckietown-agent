387a4412633aa1a008d4bada01fcbff54f0ab83d
Florian Golemo <fgolemo@gmail.com>
2018-08-07 17:12:11 -0400

    breaking bugfix. added event-based messaging instead of queue, added keyboard control

--- gym_duckietown_agent/__init__.py
+++ gym_duckietown_agent/__init__.py
@@ -6,3 +6,12 @@ register(
     timestep_limit=500,
     reward_threshold=900,
 )
+
+# this environment is the same as the one above, but doesn't have any 'print's
+register(
+    id='Duckietown-Lf-Lfv-Navv-Silent-v0',
+    entry_point='gym_duckietown_agent.envs:SimpleSimAgentEnv',
+    timestep_limit=500,
+    reward_threshold=900,
+    kwargs={"silent": True}
+)
